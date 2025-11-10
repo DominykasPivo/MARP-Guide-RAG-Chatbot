76d1c6d81fc3a80f990f6443315078d15c98da71
Dom <domas.pivoras@gmail.com>
2025-11-10 21:43:36 +0100

    Wipe branch contents

--- services/retrieval/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Pytest configuration for retrieval service tests."""
-import sys
-import os
-
-# Add the app directory to Python path
-app_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app'))
-sys.path.insert(0, app_path)
